b3dfc6e3655cb68038f29153a9e5483f7b513e92
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2023-10-12 13:50:18 +0530

    Pointers Folder created and wrote a basic program.

---
+++ Pointers/Pointers_basic.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=9;
+    int* ptr=&a;
+    cout<<"Value of the variable: "<<a<<endl;
+    cout<<"Memory location of the variable: "<<ptr<<endl;
+}
