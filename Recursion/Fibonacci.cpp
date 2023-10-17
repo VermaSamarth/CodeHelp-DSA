83ab4468ebfe1d0636fcea2063895629999701dc
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2023-10-17 17:29:19 +0530

    Recursion - Comments in Fibonacci Program.

--- Recursion/Fibonacci.cpp
+++ Recursion/Fibonacci.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int fibonacci(int num){
+    // First Number will always be 0.
     if(num == 1) return 0;
+    // Second number will always be 1.
     if(num == 2) return 1;
     int ans = fibonacci(num-1) + fibonacci(num-2);
     return ans;
