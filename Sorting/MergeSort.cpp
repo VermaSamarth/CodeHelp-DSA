6731c7150721fc084c527d86a76d651f1134116f
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2023-10-22 17:48:51 +0530

    Divide and Conquer Started: Merge 2 sorted arrays

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -2,64 +2,29 @@
 #include<vector>
 using namespace std;
 
-void merge(vector<int>& a, int start, int end){
-    int mid = (start + end) / 2;
-    int len1 = mid - start + 1;
-    int len2 = end - mid;
-    vector<int> left;
-    vector<int> right;
-    vector<int> mergeArr;
-    int k = start;
-
-    // Inserting the elements into the left sub array.
-    for(int i = 0; i < len1; i++){
-        left.push_back(a[k]);
-        k++;
-    }
-    
-    k = mid + 1;
-    // Inserting the elements into the right sub array.
-    for(int j = 0; j < len2; j++){
-        right.push_back(a[k]);
-        k++;
-    }
-    int m = 0, n = 0;
-    while(m < len1 && n < len2){
-        if(left[m] < right[n]){
-            mergeArr.push_back(left[m]);
-            m++;
-        }
-        else{
-            mergeArr.push_back(right[n]);
-            n++;
-        }
-    }
-    if(m > len1){
-        for(int i = n; i < len2; i++)
-            mergeArr.push_back(right[i]);
-    }
-    else{
-        for(int i = m; i < len1; i++)
-            mergeArr.push_back(left[i]);
-    }
-    a.clear();
-}
-
-void mergeSort(vector<int>& arr, int start, int end){
+int mergeSort(int* arr, int start, int end){
+    // Base Case
+    // if start == end -> Single element -> Already Sorted
+    // if start > end -> Invalid Array
     if(start >= end)
         return;
+
+    // Divide the array into two parts
     int mid = (start + end) / 2;
-    return mergeSort(arr, start, mid);
-    return mergeSort(arr, mid + 1, end);
-    return merge(arr, start, end);
+    
+    // Left Part divide and then Sort
+    mergeSort(arr,start,mid);
+    
+    // Rigth Part divide and then Sort
+    mergeSort(arr,mid+1,end);
+    
+    // Merge the sorted arrays
 }
 
-
-
 int main(){
-    vector<int> arr = {38,27,43,3,9,20};
-    int len = arr.size();
+    int arr[6] = {38,27,43,3,9,20};
+    int len = 6;
     int start = 0;
     int end = len - 1;
-    mergeSort(arr, start, end);
+    mergeSort(arr,start,end);
 }
