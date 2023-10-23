e5456cc3dac12fc5609d50ec722a52f63dd5424e
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2023-10-23 17:30:35 +0530

    Sortinng and Divide&Conquer: Merge Sort

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -2,7 +2,69 @@
 #include<vector>
 using namespace std;
 
-int mergeSort(int* arr, int start, int end){
+void merge(int* arr, int start, int end){
+    // Finding the middle index
+    int mid = (start + end) / 2;
+
+    // Finding the length of the arrays
+    int leftLen = mid - start + 1;
+    int rightLen = end - mid;
+
+    // Creating the Left subarray
+    int* left = new int[leftLen];
+
+    // Creating the right subarray
+    int* right = new int[rightLen];
+
+    // Copy the values into the array
+    // Left array copy
+    int k = start;
+    for(int i = 0; i < leftLen; i++){
+        left[i] = arr[k];
+        k++;
+    }
+
+    // Right Array copy
+    k = mid + 1;
+    for(int i = 0; i < rightLen; i++){
+        right[i] = arr[k];
+        k++;
+    }
+
+    // Merge Logic
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int mainIndex = start;
+    
+    while(leftIndex < leftLen && rightIndex < rightLen){
+        if(left[leftIndex] < right[rightIndex]){
+            arr[mainIndex] = left[leftIndex];
+            mainIndex++;
+            leftIndex++;
+        }
+        else{
+            arr[mainIndex] = right[rightIndex];
+            mainIndex++;
+            rightIndex++;
+        }
+    }
+    
+    // Copy Remaining elements
+    while(leftIndex < leftLen){
+        arr[mainIndex] = left[leftIndex];
+        mainIndex++;
+        leftIndex++;
+    }
+
+    while(rightIndex < rightLen){
+        arr[mainIndex] = right[rightIndex];
+        mainIndex++;
+        rightIndex++;
+    }
+    // TODO: Delete left and right created array
+}
+
+void mergeSort(int* arr, int start, int end){
     // Base Case
     // if start == end -> Single element -> Already Sorted
     // if start > end -> Invalid Array
@@ -14,11 +76,12 @@ int mergeSort(int* arr, int start, int end){
     
     // Left Part divide and then Sort
     mergeSort(arr,start,mid);
-    
+
     // Rigth Part divide and then Sort
     mergeSort(arr,mid+1,end);
-    
+
     // Merge the sorted arrays
+    merge(arr,start,end);
 }
 
 int main(){
@@ -27,4 +90,8 @@ int main(){
     int start = 0;
     int end = len - 1;
     mergeSort(arr,start,end);
+    for(int i = 0; i < len; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;  
 }
