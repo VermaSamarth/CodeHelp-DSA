136457d04b1ab70e9f658393588cf2825f3e0326
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2023-10-31 23:35:31 +0530

    OOPS

---
+++ OOPs/DynamicObjectCreation.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+
+// Creating a class  
+class Animal{
+    private:
+    int weight;
+
+    // Defined the scope of accessibility of the members of the class
+    public: 
+        // State or Properties
+        int age;
+        string name;
+
+        // Behaviour
+        void eat(){
+            cout << "Eating" << endl;
+        }
+
+        void sleep(){
+            cout << "Sleeping" << endl;
+        }
+
+        // Setters Function
+        void setWeight(int weight){
+            this->weight = weight;
+        }
+
+        // Getters Function
+        int getWeight(){
+            return weight;
+        }
+};  
+
+int main(){
+    // cout << "Size of Class Animal: " << sizeof(AnimalCat) << endl;
+    // cout << "Hello World" << endl;
+
+    // Creating an Object for the class
+    // Static Memory
+    // Animal Ramesh;
+    // cout << "Name of Ramesh is " << Ramesh.name << endl;
+    // cout << "Age of Ramesh is " << Ramesh.age << endl;
+
+    // // Access or Update the properties of the Class
+    // Ramesh.age = 20;
+    // Ramesh.name = "Dog";
+    // cout << "Name of Ramesh is " << Ramesh.name << endl;
+    // cout << "Age of Ramesh is " << Ramesh.age << endl;
+
+    // Ramesh.eat();
+    // Ramesh.sleep();
+
+    // // Calling private member of the class using getter and setter
+    // Ramesh.setWeight(100);
+    // cout << "Weight of Ramesh is " << Ramesh.getWeight() << endl;
+
+
+    // Dynamic Memory
+
+     
+}
