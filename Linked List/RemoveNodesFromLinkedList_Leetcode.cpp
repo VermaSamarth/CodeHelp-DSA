8da48ec7cad903e30d2cf2754e22a21e8419cb8f
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2024-01-26 17:10:24 +0530

    ....

---
+++ Linked List/RemoveNodesFromLinkedList_Leetcode.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(int data){
+            this->data = data;
+            this->next = nullptr;
+        }
+
+        ~Node(){
+            cout << "Node deleted successfully!!!" << endl;
+        }
+};
+
+Node* reverseList(Node* & head){
+    Node* current = head;   
+    Node* previous = nullptr;
+    Node* forward ;
+    while(current != nullptr){
+        forward = current->next;
+        current->next = previous;
+        previous = current;
+        current = forward;
+    }
+    return previous;
+}
+
+void InsertStart(int data, Node* &head){
+    if(head == NULL){
+        Node* newNode = new Node(data);
+        head = newNode;
+        return;
+    }
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+    return;    
+}
+
+void printList(Node* &head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+    return;
+}
+
+Node* removeNode(Node* &head){
+    stack<Node*> stk;
+    Node* temp = head;
+    // int maxval = INT_MIN;
+    stk.push(temp);
+    while(temp != nullptr){
+        //maxval = temp->data;
+        if(temp == stk.top()){
+
+        }
+    }
+}
+
+int main(){
+    Node* head = nullptr;
+    InsertStart(70,head);
+    InsertStart(60,head);
+    InsertStart(50,head); 
+    InsertStart(40,head);
+    InsertStart(30,head);
+    InsertStart(20,head);
+    InsertStart(10,head);
+    cout << "Linked List: " << endl;
+    printList(head);
+    Node* addr = removeNode(head);
+    cout << "Linked List: " << endl;
+    printList(addr);
+}
