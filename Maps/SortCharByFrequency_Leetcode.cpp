32b32adeae1963326f5b609ec3c6f80570259b75
samarthverma1813@gmail.com <samarthverma1813@gmail.com>
2024-02-07 19:28:38 +0530

    Sort character by frequency updated

--- Maps/SortCharByFrequency_Leetcode.cpp
+++ Maps/SortCharByFrequency_Leetcode.cpp
@@ -7,14 +7,20 @@ using namespace std;
 int main(){
     string str = "ababacdcdekfghs";
     string ansstr = "";
-    priority_queue<pair<int,char>> nums;
+    // Map to find the count for the occurence of each character.
     map<char,int> mp;
+    // Priority queue to store the data in the stored order.
+    // Data stored in the priority queue is in the form of the pairs.
+    priority_queue<pair<int,char>> nums;
+    // Storing the occurence of each character and counter in the map.
     for(int i = 0; i < str.length(); i++){
         mp[str[i]]++;
     }
+    // Sending the values of the map to priority queue.
     for(auto& it : mp)
         nums.push(make_pair(it.second,it.first));
 
+    // Popping the queue elements and generating the new string.
     while(!nums.empty()){
         int size = nums.top().first;
         while(size != 0){
